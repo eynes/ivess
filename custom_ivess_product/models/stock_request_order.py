23214a9677975a1b96eeb7f16153a0182dd6b128
Agustin Bordon <agustin.bordon@eynes.com.ar>
2026-03-13 09:24:52 -0300

    [T13391] Cambios post pedidos de existencia

--- custom_ivess_product/models/stock_request_order.py
+++ custom_ivess_product/models/stock_request_order.py
@@ -30,13 +30,43 @@ class StockRequestOrder(models.Model):
 class StockRequest(models.Model):
     _inherit = "stock.request"
 
+    # def _action_confirm(self):
+    #     res = super()._action_confirm()
+
+    #     if self.location_id:
+    #         pickings = self.picking_ids
+    #         if pickings:
+    #             pickings.write({'location_dest_id': self.location_id.id})
+    #             pickings.move_ids.write({'location_dest_id': self.location_id.id})
+    #             pickings.move_line_ids.write({'location_dest_id': self.location_id.id})
+    #     return res
+
     def _action_confirm(self):
         res = super()._action_confirm()
-
         if self.location_id:
             pickings = self.picking_ids
             if pickings:
-                pickings.write({'location_dest_id': self.location_id.id})
-                pickings.move_ids.write({'location_dest_id': self.location_id.id})
-                pickings.move_line_ids.write({'location_dest_id': self.location_id.id})
+                pickings.write({
+                    'location_dest_id': self.location_id.id,
+                    'manual_location': True
+                })
+                pickings.move_ids.write(
+                    {'location_dest_id': self.location_id.id}
+                )
+                if pickings.move_line_ids:
+                    pickings.move_line_ids.write(
+                        {'location_dest_id': self.location_id.id}
+                    )
         return res
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    manual_location = fields.Boolean(readonly=True)
+
+    @api.depends('picking_type_id', 'partner_id', 'manual_location')
+    def _compute_location_id(self):
+        manual_pickings = self.filtered(lambda p: p.manual_location)
+        standard_pickings = self - manual_pickings
+        if standard_pickings:
+            super(StockPicking, standard_pickings)._compute_location_id()
