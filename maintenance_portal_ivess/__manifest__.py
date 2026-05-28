d47a59b24ab39317ded21d6b1c3f3f1b14275bb3
nicolas.russo <nicolas.russo@eynes.com.ar>
2026-05-28 16:33:49 -0300

    T14588 07:00 Meet Rocio + Workshop and maintenance menus added to portal view.

---
+++ maintenance_portal_ivess/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Maintenance Portal - Ivess',
+    'version': '19.0.0.0.0',
+    'author': 'Eynes',
+    'category': 'Maintenance',
+    'summary': 'Vista portal para órdenes de mantenimiento',
+    'description': """
+        Provee una vista portal para usuarios con acceso web que permite:
+        - Ver todas las órdenes de mantenimiento en una tabla paginada.
+        - Acceder al detalle de cada solicitud de mantenimiento.
+    """,
+    'depends': [
+        'portal',
+        'maintenance',
+    ],
+    'data': [
+        'views/maintenance_portal_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
