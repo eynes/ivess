75ae1b5e320a49226745f168c7663079b09721bf
Agustin Bordon <agustin.bordon@eynes.com.ar>
2026-06-09 12:54:34 -0300

    [T14876] Migra de v17 a v19 campos en res.partner, nuevo modulo ivess_partner_custom

---
+++ ivess_partner_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Ivess Partner Custom",
+    "version": "19.0.0.0.1",
+    "description": "",
+    "author": "Eynes",
+    "category": "Contacts",
+    "depends": [
+        "base",
+        "sale",
+        "account",
+        "l10n_ar_eynes",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
