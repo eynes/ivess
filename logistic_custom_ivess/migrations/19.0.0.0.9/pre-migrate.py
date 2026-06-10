b013101ab152cf90cef880a1e01d67bd630d366a
Agustin Bordon <agustin.bordon@eynes.com.ar>
2026-06-10 14:18:29 -0300

    [T14688] Modifica a campo computado la fecha de asignacion de envase

---
+++ logistic_custom_ivess/migrations/19.0.0.0.9/pre-migrate.py
@@ -0,0 +1,9 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # assignment_date pasa a ser campo computado: quitar la restricción NOT NULL
+    cr.execute("""
+        ALTER TABLE water_container
+        ALTER COLUMN assignment_date DROP NOT NULL
+    """)
