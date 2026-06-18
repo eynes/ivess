3fd0504ab251af4e5321df3c85cfacb4930c6fb1
Agustin Bordon <agustin.bordon@eynes.com.ar>
2026-06-18 10:32:58 -0300

    HOT FIX SERVICE FREE OF CHAR REPORT

--- ivess_webservice/models/ivess_limit_free_of_charge_report.py
+++ ivess_webservice/models/ivess_limit_free_of_charge_report.py
@@ -4,5 +4,16 @@ class IvessLimitFreeOfChargeReport(models.Model):
     _name = "ivess.limit.free.of.charge.report"
     _description = "Vista SQL de límites de sin cargo expuesta al middleware Ivess"
     _auto = False
-    
+
     default_code = fields.Char(readonly=True)
+
+    def init(self):
+          tools.drop_view_if_exists(self.env.cr, self._table)
+          self.env.cr.execute("""
+              CREATE OR REPLACE VIEW ivess_limit_free_of_charge_report AS (
+                  SELECT
+                      id,
+                      default_code
+                  FROM product_template
+              )
+          """)
