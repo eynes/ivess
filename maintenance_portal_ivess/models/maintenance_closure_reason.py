9c658e7f4bd5a9aba102fb5e6094be7b17241071
nicolas.russo <nicolas.russo@eynes.com.ar>
2026-06-29 15:05:49 -0300

    T14588 03:30 Maintenance changes.

--- maintenance_portal_ivess/models/maintenance_closure_reason.py
+++ maintenance_portal_ivess/models/maintenance_closure_reason.py
@@ -8,3 +8,24 @@ class MaintenanceClosureReason(models.Model):
     _order = 'name'
 
     name = fields.Char(string='Motivo', required=True)
+
+    parent_id = fields.Many2one(
+        'maintenance.closure.reason',
+        string='Categoría padre',
+        ondelete='restrict',
+        index=True,
+    )
+    child_ids = fields.One2many(
+        'maintenance.closure.reason',
+        'parent_id',
+        string='Subcategorías',
+    )
+    is_parent = fields.Boolean(
+        string='Es categoría',
+        compute='_compute_is_parent',
+        store=True,
+    )
+
+    def _compute_is_parent(self):
+        for rec in self:
+            rec.is_parent = not rec.parent_id
