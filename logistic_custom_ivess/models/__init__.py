db91200405266114055a1b9c1d3dd8cfe8459f5e
pluque <pedro.luque@eynes.com.ar>
2026-07-03 09:36:07 -0300

    [T15250] 01:00 Agregar campo 'minutos por convertir a factura' en ajustes de facturacion

--- logistic_custom_ivess/models/__init__.py
+++ logistic_custom_ivess/models/__init__.py
@@ -24,3 +24,4 @@ from . import stock_location
 from . import stock_move
 from . import stock_picking
 from . import sale_report
+from . import res_config_settings
---
+++ logistic_custom_ivess/models/res_config_settings.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    minutos_x_convertir_factura = fields.Float(
+        string='Minutos por convertir a factura',
+    )
