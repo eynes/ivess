73c2273ab9af6799af1fcbeaea85115ee9ee08db
Agustin Bordon <agustin.bordon@eynes.com.ar>
2026-08-20 10:16:06 -0300

    [T16058] 01:10 Agrega onchange para sugerir cheque en el wizard de cheques propios

---
+++ ivess_check_next_number/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Ivess Check Next Number",
+    "version": "19.0.0.0.0",
+    "summary": "Autocompleta el próximo número de cheque disponible al elegir la chequera",
+    "description": """
+        En el wizard "Crear Cheques propios" (emisión desde Orden de Pago),
+        al seleccionar la Chequera el campo Cheque se autocompleta con el
+        próximo número disponible (el más bajo libre) de esa chequera, en
+        vez de exigir la búsqueda manual en el desplegable. El usuario
+        puede seguir cambiándolo manualmente por excepción (cheque
+        salteado, anulado, o fuera de orden).
+    """,
+    "author": "Eynes",
+    "category": "Accounting",
+    "depends": [
+        "l10n_ar_eynes",
+    ],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
