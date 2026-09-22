79a3d651d0464d2c4e2a855b2788e0e7fb4ce9c0
Agustin Bordon <agustin.bordon@eynes.com.ar>
2026-09-22 09:05:25 -0300

    [T16590] 01:00 Agrega modulo custom para no tocar loc

---
+++ account_bank_rec_widget_ivess/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Account Bank Reconciliation Widget Ivess",
+    "version": "19.0.1.0.0",
+    "author": "Eynes",
+    "category": "Accounting/Accounting",
+    "depends": [
+        "account_accountant",
+    ],
+    "data": [],
+    "demo": [],
+    "assets": {
+        "web.assets_backend": [
+            "account_bank_rec_widget_ivess/static/src/js/bank_rec_reconcile_filters.js",
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ account_bank_rec_widget_ivess/readme/DESCRIPTION.md
@@ -0,0 +1,6 @@
+Personalizaciones de Ivess sobre el widget de conciliacion bancaria
+(`account_accountant`, Enterprise).
+
+En el dialogo "Buscar: Apuntes contables por conciliar" saca los
+asientos que Odoo genera automaticamente por cada linea de extracto
+bancario: nunca son una contrapartida valida de otra transaccion.
